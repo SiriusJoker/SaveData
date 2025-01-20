46657f313584c2f923eca4cbb764d02ba6ee8dc5
SiriusJoker <kortukovaa@gmail.com>
2025-01-20 17:36:17 +0100

    some refactoring done. Still work

--- Save_data_to_json.py
+++ Save_data_to_json.py
@@ -2,7 +2,9 @@ import maya.cmds as cmds
 import json
 import os
 from datetime import datetime
+from collections import namedtuple
 
+out_name_path=namedtuple('out_name_path',['raw_name','output_path'])
 
 def load_config(config_path):
     try:
@@ -12,35 +14,20 @@ def load_config(config_path):
         print(f"Error loading configuration: {e}")
         return {}
 
+def get_output_path(filepath):
 
-def export_poly_data_to_json():
-
-    script_directory = cmds.internalVar(userScriptDir=True)
-
-    config_path = os.path.join(script_directory, "SaveData/config.json") 
-    config=load_config(config_path)
-    
-    # Get current scene
-    filepath = cmds.file(q=True, sn=True)
-    if not filepath:
-        print("No scene file is currently open. Maybe you need to save the scene and try again")
-        return
-    
     # Get name and path to output file
     filename = os.path.basename(filepath)
     raw_name = os.path.splitext(filename)[0]
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    
+
     workspace_dir = cmds.workspace(query=True, rd=True)
     export_folder = os.path.join(workspace_dir, "ExportedData")
-    os.makedirs(export_folder, exist_ok=True)  
-    output_path = os.path.join(export_folder, f"scene_poly_data_{raw_name}_{timestamp}.json")
-    
-    # Get data for polygonal objects
-    poly_objects = cmds.ls(type="mesh", long=True)
-    if not poly_objects:
-        print("No polygonal objects found in the scene.")
-    
+    os.makedirs(export_folder, exist_ok=True)
+    output_path=os.path.join(export_folder, f"scene_poly_data_{raw_name}_{timestamp}.json")
+    return out_name_path(raw_name, output_path)
+
+def get_poly_data(poly_objects,config):
     data_obj = {}
     for obj in poly_objects:
         try:
@@ -56,7 +43,7 @@ def export_poly_data_to_json():
             children_count = len(children)
             parentt = cmds.listRelatives(transform, parent=True, fullPath=True)
             groups = parentt[0] if parentt and cmds.objectType(parentt[0]) else None
-            
+
             obj_data = {}
             if config.get("save_vertex_count", False):
                 obj_data["vertex_count"] = vertex_count
@@ -76,11 +63,36 @@ def export_poly_data_to_json():
                 obj_data["display_layers"] = display_layers if display_layers else 0
             if config.get("save_groups", False):
                 obj_data["groups"] = groups
-            
+
             data_obj[transform] = obj_data
         except Exception as e:
             print(f"Error processing object {obj}: {e}")
             continue
+    return data_obj
+
+def export_poly_data_to_json():
+
+    script_directory = cmds.internalVar(userScriptDir=True)
+
+    config_path = os.path.join(script_directory, "SaveData/config.json") 
+    config=load_config(config_path)
+
+    # Get current scene
+    filepath = cmds.file(q=True, sn=True)
+    if not filepath:
+        print("No scene file is currently open. Maybe you need to save the scene and try again")
+        return
+
+    out_name_path=get_output_path(filepath)
+    raw_name, output_path=out_name_path;
+
+    # Get data for polygonal objects
+    poly_objects = cmds.ls(type="mesh", long=True)
+    if not poly_objects:
+        print("No polygonal objects found in the scene.")
+
+    data_obj = get_poly_data(poly_objects,config)
+
     
     # Get data for scene
     file_size = os.path.getsize(filepath)
